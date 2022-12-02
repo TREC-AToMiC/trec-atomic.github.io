a6168f96c802723900b2eba6daace4451167c9d7
justram <jheng-hong.yang@uwaterloo.ca>
2022-12-02 16:07:20 -0500

    cleanup files

--- docs/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "AToMiC (v0.1) Dataset Released",
-        "excerpt":"AToMiC (v0.1) Dataset is avaliable on HuggingFace Hub   Requirements     HuggingFace Datasets   Code snippets:  from datasets import load_dataset  dataset = load_dataset( \t\t\"TREC-AToMiC/AToMiC-Images-v0.1\", \t\tsplit='train' \t  ) print(dataset)  ","categories": ["Annoucements"],
-        "tags": ["Dataset"],
-        "url": "/annoucements/huggingface-hub-released/",
-        "teaser": null
-      }]
